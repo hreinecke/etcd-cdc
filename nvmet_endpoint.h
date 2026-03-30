e24b6587bc98dc9d80c2999555a60801a645abfc
Hannes Reinecke <hare@suse.de>
2026-03-30 11:55:52 +0200

    Remove stale files

--- nvmet_endpoint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _NVMET_ENDPOINT_H
-#define _NVMET_ENDPOINT_H
-
-void *endpoint_thread(void *arg);
-struct endpoint *enqueue_endpoint(int id, struct host_iface *iface);
-void dequeue_endpoint(struct endpoint *ep);
-
-#endif /* _NVMET_ENDPOINT_H */
-
